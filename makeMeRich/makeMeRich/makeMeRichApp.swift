c2429b624218b1a4cef3468ad7a5487349afceba
Kasra Zanganeh <kasra.zanganeh@hotmail.com>
2024-08-09 21:27:40 -0700

    first commit

---
+++ makeMeRich/makeMeRich/makeMeRichApp.swift
@@ -0,0 +1,17 @@
+//
+//  makeMeRichApp.swift
+//  makeMeRich
+//
+//  Created by Kasra Zanganeh on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct makeMeRichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
