41d48446b31a87ad79cbf5ae68b48d3fa5671c87
Kasra Zanganeh <kasra.zanganeh@hotmail.com>
2025-03-03 19:57:16 -0800

    another commit

---
+++ makeMeRich2/makeMeRich2/makeMeRich2App.swift
@@ -0,0 +1,17 @@
+//
+//  makeMeRich2App.swift
+//  makeMeRich2
+//
+//  Created by Kasra Zanganeh on 11/17/24.
+//
+
+import SwiftUI
+
+@main
+struct makeMeRich2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ makeMeRich2/makeMeRich2Tests/makeMeRich2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  makeMeRich2Tests.swift
+//  makeMeRich2Tests
+//
+//  Created by Kasra Zanganeh on 11/17/24.
+//
+
+import Testing
+@testable import makeMeRich2
+
+struct makeMeRich2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
